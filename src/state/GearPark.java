8c553df1031194cc8c55e090d7e3f6a42f197cda
woodrmichael <woodm@msoe.edu>
2024-12-03 13:25:03 -0600

    GearPark

--- src/state/GearPark.java
+++ src/state/GearPark.java
@@ -4,12 +4,19 @@ public class GearPark implements Gear {
 
     @Override
     public Gear nextGear(UserInput ui) {
-        return null;
+        Gear gear = this;
+        if (ui.checkTapped("UP")) {
+            gear = new GearReverse();
+        }
+        return gear;
     }
 
     @Override
     public void resolveBehavior(UserInput ui, Car car) {
-
+        int currentSpeed = car.getSpeed();
+        if (currentSpeed > 0) {
+            car.changeSpeed(--currentSpeed);
+        }
     }
 
     @Override
