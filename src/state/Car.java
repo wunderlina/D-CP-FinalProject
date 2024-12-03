cf1a96573c097cf1fb517d01a2684bab17baf4ec
woodrmichael <woodm@msoe.edu>
2024-12-03 13:32:59 -0600

    Car updated to set User Input

--- src/state/Car.java
+++ src/state/Car.java
@@ -4,31 +4,39 @@ public class Car {
     private Gear gear;
     private int speed;
     private boolean stalled;
-    private UserInput userInput;
+    private final UserInput userInput;
 
     public Car(UserInput ui){
+        userInput = ui;
         gear = new GearPark();
         stalled = true;
     }
+
     public void changeSpeed(int increment){
         speed += increment;
     }
+
     public void setStalled(boolean bool){
         stalled = bool;
     }
+
     public int getSpeed(){
         return speed;
     }
+
     public boolean getStalled(){
         return stalled;
     }
+
     private void shiftGears(){
         this.gear = gear.nextGear(userInput);
     }
+
     public void update(){
         gear.resolveBehavior(userInput, this);
         shiftGears();
     }
+
     public int getGear(){
         return gear.getGear();
     }
