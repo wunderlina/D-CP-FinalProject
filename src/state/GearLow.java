a1ff8963fc8e9cbe1eaab7d3c20fbf227ed71cf3
cocorowachi <113720641+cocorowachi@users.noreply.github.com>
2024-12-03 14:27:36 -0600

    modify gearhigh and wrote gearlow behavior

--- src/state/GearLow.java
+++ src/state/GearLow.java
@@ -3,12 +3,26 @@ package state;
 public class GearLow implements Gear {
     @Override
     public Gear nextGear(UserInput ui) {
-        return null;
+        Gear gear = this;
+        if (ui.checkTapped("UP")) {
+            gear = new GearHigh();
+        }
+        if (ui.checkTapped("DOWN")) {
+            gear = new GearNeutral();
+        }
+        return gear;
     }
 
     @Override
     public void resolveBehavior(UserInput ui, Car car) {
-
+//max velocity 50
+        int currentSpeed = car.getSpeed();
+        if (currentSpeed < 50 && ui.getKeyPressed("SPACE")){
+            car.changeSpeed(8);
+        }
+        else {
+            car.changeSpeed(-3);
+        }
     }
 
     @Override
